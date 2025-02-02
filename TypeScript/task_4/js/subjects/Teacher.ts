ced2f77fd968fa111a7b12d2da13c3bd5d61137c
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2025-02-02 20:04:52 +0000

    task 4

---
+++ TypeScript/task_4/js/subjects/Teacher.ts
@@ -0,0 +1,8 @@
+namespace Subjects {
+    export interface Teacher {
+        firstName: string,
+        lastName: string
+        experienceTeachingReact?: number;
+        experienceTeachingJava?: number;
+    }
+}
